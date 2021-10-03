c75739edaa60bb4feb0606380f41db6473b9ae7c
Moshe tmv <moshikot015@gmail.com>
2021-10-03 15:19:13 +0300

    .

---
+++ Example.ps1
@@ -0,0 +1,8 @@
+﻿Invoke-WebRequest -Uri http://localhost:8080/ -Method POST -Body $Message
+
+
+#Web get english format
+#http://localhost:8080/send?destination=972544911249@c.us&message=test&token=8s8d9s9fs991
+
+
+$Message = '{"destination": "0000000000@c.us","message": "sup", "token": "8s8d9s9fs991"}'
---
+++ simple.js
@@ -0,0 +1,298 @@
+ const http = require("http");
+const url = require('url');
+const fs = require('fs');
+
+const request = require('request');
+
+const qrcode = require("qrcode-terminal");
+const { Client } = require('whatsapp-web.js');
+
+const SESSION_FILE_PATH = './session.json';
+
+let client;
+
+const spam = true;
+const checkToken = true;
+
+
+const chatBot = true;
+const chatBotURL = "http://localhost/";
+let sessionData;
+
+let sessao;
+let sessobj;
+
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    //sessionData = require(SESSION_FILE_PATH);
+
+    sessao = fs.readFileSync(SESSION_FILE_PATH);
+    sessobj = JSON.parse(sessao);
+
+
+    //console.log(sessobj);
+    //console.log(sessao.toString());
+    //return;
+
+    client = new Client({
+      session: sessobj
+    });
+
+} else {
+  client = new Client();
+}
+
+
+
+
+
+
+const QRCode = require('qrcode');
+
+estado = 0;
+
+let autorizadosArquivo = fs.readFileSync("./autorizados.json");
+let autorizados = JSON.parse(autorizadosArquivo);
+
+let token = JSON.parse(fs.readFileSync("./token.json"));
+/*
+function redirectChatBot(timestamp, sender, message){
+
+  try {
+
+  request(chatBotURL +"?timestamp=" +timestamp +"&sender=" +sender +"&message=" +message, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log("Forwarded to ChatBot API")
+     } else {
+       console.log("Chatbot did not recieve the message");
+     }
+})
+
+  } catch(e) {
+    console.log("Error calling chatbot API");
+  }
+  
+
+}
+*/
+
+function checaToken(__token){
+
+
+  
+    if (token.token == __token) {
+      console.log(__token);
+      return true;
+    } else {
+      return false;
+    }
+  
+
+
+}
+
+
+function checaAutorizados(__numero) {
+
+  if (spam == true){
+    autorizaNumero(__numero);
+  }
+
+    for (x = 0; x < autorizados.length; x++) {
+      if (autorizados[x].numero === __numero) {
+        return autorizados[x];
+      }
+    }
+    return autorizados[0];
+  }
+
+
+function autorizaNumero(__numero) {
+    autorizados.push({ numero: __numero});
+    let data = JSON.stringify(autorizados);
+    fs.writeFileSync("./autorizados.json", data);
+
+  }
+
+unread = [];
+
+client.on('qr', (qr) => {
+    estado = 0;
+    console.log("Waiting for QRCode reading", qr);
+    qrcode.generate(qr, {small: true});
+    QRCode.toFile('qrcode.png', qr);
+
+});
+
+client.on('ready', () => {
+    estado = 1;
+    console.log('Ready');
+});
+
+client.on('message', async (msg) => {
+    if(msg.body === 'קובי תן תיוג') {
+		
+        const chat = await msg.getChat();
+		//Get sender name
+		//const author = await msg.getContact();
+        
+        let text = "";
+        let mentions = [];
+
+        for(let participant of chat.participants) {
+            const contact = await client.getContactById(participant.id._serialized);
+            
+            mentions.push(contact);
+            text += `@${participant.id.user} `;
+        }
+
+        await msg.reply("מתייג את כולם:\n"+text,"", { mentions });
+    }
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
+	}
+});
+
+client.on('message', async (msg) => {
+    
+    console.log('Message from: ', msg.from, " - ", msg.body);
+    const author = await msg.getContact();
+    let date = new Date();
+    
+    unread.push({ timestamp: date.getTime(), sender: msg.from, sendername: author.pushname ,message: msg.body });
+
+
+    if (checaAutorizados(msg.from).numero == "00000000000"){
+        console.log("Number " +msg.from +" Successfully authorized!");
+      autorizaNumero(msg.from);
+      }
+
+});
+
+
+ client.initialize();
+
+
+ client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+      if (err) {
+          console.error(err);
+      }
+  });
+});
+
+
+const server = http.createServer(function(req, res){
+    x = url.parse(req.url, { parseQueryString: true });
+
+
+//    let ip = req.header('x-forwarded-for') || ;
+
+
+    //console.log(req.connection.remoteAddress);
+
+    
+    if (req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+          body += chunk.toString(); // convert Buffer to string
+      });
+      req.on('end', () => {
+          ___numero = JSON.parse(body).destination +"";
+          ___message = JSON.parse(body).message +"";
+          ___token = JSON.parse(body).token;
+
+          console.log(checaToken(___token));
+  
+
+          if (checaAutorizados(___numero).numero == "00000000000"){
+            console.log("Attempt to send with invalid token via POST");
+            //res.write("Attempt to send with invalid token via POST");
+            //res.end();
+          } else if (checaAutorizados(___numero).numero == ___numero){
+            //res.write(___numero + " - " +___message);
+
+            if (checaToken(___token)){
+            client.sendMessage(___numero, ___message);
+            } else {
+              console.log(req.connection.remoteAddress + " - Attempt to send with invalid token via POST");
+            }
+            //res.end();
+          }
+
+          
+      });
+  }
+
+  
+
+    if (x.query.destination){
+
+        ___numero = x.query.destination +"";
+        ___message = x.query.message +"";
+
+        ___token = x.query.token;
+
+        console.log(checaToken(___token));
+
+        if (checaAutorizados(___numero).numero == "00000000000"){
+            console.log("Unauthorized number, inform your customer to send a message to authorize.");
+            res.write("Unauthorized number, inform your customer to send a message to authorize.");
+            res.end();
+          } else if (checaAutorizados(___numero).numero == ___numero){
+
+            if (checaToken(___token)){
+              client.sendMessage(___numero, ___message);
+              res.write(___numero + " - " +___message);
+              } else {
+                console.log(req.connection.remoteAddress + " - Attempt to send with invalid token via GET");
+                res.write(req.connection.remoteAddress + " - Attempt to send with invalid token via GET");
+              }
+
+            //client.sendMessage(___numero, ___message);
+            res.end();
+          }
+
+
+            //console.log(x.query.destination + " - " +x.query.message);
+            
+    } else {
+        switch (req.url){
+            case "/unread":
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.write(JSON.stringify(unread));
+               // res.write(mostraRecebidas());
+				unread = [];
+
+                break;
+            case "/":
+                
+                if (estado == 0){
+                    // res.write("Mostra qrcode");
+
+                    var img = fs.readFileSync('./qrcode.png');
+                    res.writeHead(200, {'Content-Type': 'image/png' });
+                    res.end(img, 'binary');
+
+                } else if (estado == 1){
+                    res.write("Already connected");
+                }
+                
+                break;
+            default:
+                res.write("Wrong path");
+        }
+        try {
+        res.end();
+        } catch(e){
+            console.log("Nothing to do");
+        }
+    }
+});
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+
