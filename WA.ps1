a64a44ab4ddb8c382e4147d8f0c492b9cb246be5
Moshikot3 <moshikot015@gmail.com>
2022-03-24 18:21:05 +0200

    Update WA.ps1

--- WA.ps1
+++ WA.ps1
@@ -5,6 +5,6 @@
 #http://localhost:8080/send?destination=972544911249@c.us&message=test&token=8s8d9s9fs991
 
 
-$Message = '{"destination": "972544911249@c.us","message": "sup", "token": "8s8d9s9fs991", "messageid": "false_972544911249@c.us_3AE17BEEC99938ED9F6D"}'
+$Message = '{"destination": "972544231245@c.us","message": "sup", "token": "8s8d9s9fs991", "messageid": "false_972544911249@c.us_3AE17BEEC99938ED9F6D"}'
 
-$Message = '{"destination": "972544911249@c.us","message": "sup", "token": "8s8d9s9fs991"}'
+$Message = '{"destination": "972544231245@c.us","message": "sup", "token": "8s8d9s9fs991"}'
